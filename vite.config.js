d417c0efc3512252e2bc42169c22f222d8450836
Belen Hervas <belen.hervas01@gmail.com>
2025-11-02 12:05:36 -0500

    update Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: "/FormulaKentucky/",
+  plugins: [react()],
   server: {
     open: true,
     port: 3001,
